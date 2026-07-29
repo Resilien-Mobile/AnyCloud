ba4f117fc4615d05051c9baf81d72ee112bf095b
Creater. Bai <1302064396@qq.com>
2026-07-29 18:16:46 +0800

    fix(notification): Cancel ongoing notifications when deleting configs
    Cancel the posted notification before removing its stored config.
    Keep system notifications and local notification records in sync.

--- app/src/main/java/com/baidaidai/anycloud/application/notification/DeleteOneOngoingNotificationConfigUseCase.kt
+++ app/src/main/java/com/baidaidai/anycloud/application/notification/DeleteOneOngoingNotificationConfigUseCase.kt
@@ -1,11 +1,19 @@
 package com.baidaidai.anycloud.application.notification
 
+import com.baidaidai.anycloud.data.notification.gateway.NotificationGatewayImpl
 import com.baidaidai.anycloud.data.notification.repository.NotificationRepositoryImpl
 import com.baidaidai.anycloud.domain.notification.model.NotificationConfig
 import javax.inject.Inject
 
 class DeleteOneOngoingNotificationConfigUseCase @Inject constructor(
-    private val notificationRepositoryImpl: NotificationRepositoryImpl
+    private val notificationRepositoryImpl: NotificationRepositoryImpl,
+    private val notificationGatewayImpl: NotificationGatewayImpl
 ) {
-    suspend operator fun invoke(notificationConfig: NotificationConfig) = notificationRepositoryImpl.deleteOneNotification(notificationConfig)
+    suspend operator fun invoke(notificationConfig: NotificationConfig) {
+        // Cancel One Ongoing Notification
+        notificationGatewayImpl.cancelOneOngoingNotification(notificationConfig)
+
+        // Delete One Ongoing Notification Config
+        notificationRepositoryImpl.deleteOneNotification(notificationConfig)
+    }
 }
