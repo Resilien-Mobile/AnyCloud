d10edb6246028de9928d1aeddc5d63c6f58f6e4c
Creater. Bai <1302064396@qq.com>
2026-09-22 16:32:59 +0800

    refactor(energy): Add raw energy datasource contract
    Define a domain contract for raw ampere and voltage streams.
    
    Add explicit ampere and voltage unit models.
    
    Allow domain energy contracts to expose coroutine flows.

---
+++ domain/src/main/kotlin/com/baidaidai/anycloud/domain/energy/model/AmpereUnit.kt
@@ -0,0 +1,13 @@
+package com.baidaidai.anycloud.domain.energy.model
+
+enum class AmpereUnit {
+
+    /**
+     * 1 mA = 1000 μA
+     */
+
+    // 毫安培
+    MILLIAMPERE,
+    // 微安培
+    MICROAMPERE
+}
